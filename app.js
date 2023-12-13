12670f6da1d379de98cb9e0c98ebecf8eba3e562
Anton1o <85672368+Murad623@users.noreply.github.com>
2023-12-13 15:48:05 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 // const value = fs.readFile("./data.json",'utf8', (err, data) => {
 const value = fs.readFileSync("./data.json",'utf8', (err, data) => {
     if (!err) {
-        return JSON.parse(data)
+        return data
         
     }
     else {
@@ -16,9 +16,14 @@ const express = require("express");
 const app = express()
   
 app.get('/', function (req, res) {
-    res.send(`Name : ${value[0].name} | ID : ${value[0].id} | Age : ${value[0].age} /
-    Name : ${value[1].name} | ID : ${value[1].id} | Age : ${value[1].age} /
-    Name : ${value[2].name} | ID : ${value[2].id} | Age : ${value[2].age}`)
+    res.send(`Name : ${JSON.parse(value)[0].name} | ID : ${JSON.parse(value)[0].id} | Age : ${JSON.parse(value)[0].age} /
+    Name : ${JSON.parse(value)[1].name} | ID : ${JSON.parse(value)[1].id} | Age : ${JSON.parse(value)[1].age} /
+    Name : ${JSON.parse(value)[2].name} | ID : ${JSON.parse(value)[2].id} | Age : ${JSON.parse(value)[2].age}`)
+});
+app.get('/:id',function(req,res){
+    const id = req.params.id
+    const emp = JSON.parse(value).find(emp => emp.id == id)
+    res.send(`Name : ${emp.name} <br> ID : ${emp.id} <br> Age : ${emp.age}`)
 });
 
 app.listen(3000, function () {
