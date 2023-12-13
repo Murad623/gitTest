f277e69c00f4d79c762ff9e6d2e591f826a5b4bc
Anton1o <85672368+Murad623@users.noreply.github.com>
2023-12-13 16:57:24 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -16,14 +16,9 @@ const express = require("express");
 const app = express()
   
 app.get('/', function (req, res) {
-    res.send(`Name : ${JSON.parse(value)[0].name} | ID : ${JSON.parse(value)[0].id} | Age : ${JSON.parse(value)[0].age} /
-    Name : ${JSON.parse(value)[1].name} | ID : ${JSON.parse(value)[1].id} | Age : ${JSON.parse(value)[1].age} /
+    res.send(`Name : ${JSON.parse(value)[0].name} | ID : ${JSON.parse(value)[0].id} | Age : ${JSON.parse(value)[0].age} <br>
+    Name : ${JSON.parse(value)[1].name} | ID : ${JSON.parse(value)[1].id} | Age : ${JSON.parse(value)[1].age} <br>
     Name : ${JSON.parse(value)[2].name} | ID : ${JSON.parse(value)[2].id} | Age : ${JSON.parse(value)[2].age}`)
-});
-app.get('/:id',function(req,res){
-    const id = req.params.id
-    const emp = JSON.parse(value).find(emp => emp.id == id)
-    res.send(`Name : ${emp.name} <br> ID : ${emp.id} <br> Age : ${emp.age}`)
 });
 
 app.listen(3000, function () {
